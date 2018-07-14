14dd5f8716d2aa7333a59dc0966ad69f08802b76
Olegzandr Denman <alex@oresoftware.com>
2018-07-14 14:14:39 -0700

    init

---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+
+const rolo = require('rolo2');
+const yolo = require('yolo3');
+
+assert.equal(rolo.default, 2);
+assert.equal(yolo.default, 3);
